3c0fd0af5579c0fe4a13235234b890100858ee34
Aryan Mishra <aryan456.rehla@gmail.com>
2025-12-25 21:54:57 +0530

    student list initialisation

---
+++ src/Pages/teacher/Studentlist.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { studentProgressData } from '@/data/dummy'
+import { Table, TableHeader, TableBody, TableRow, TableCell,
+    TableHead, TableBody
+ } from '@/Components/ui/table'
+ import { Avatar, AvatarFallback, AvatarImage, Badge } from '@/Components/ui/avatar'
+
+const Studentlist = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Studentlist
